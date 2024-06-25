eafe1c1668b2d8e743b900e2c796d026f9287620
Pavel Sytnik <pashasytnik@gmail.com>
2024-06-25 23:05:09 +0400

    Add vector header

---
+++ vector.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+struct vector;
+
+struct vector *vector_create();
+void vector_destroy(struct vector *vec);
+
+int vector_is_empty(struct vector* vec);
+int vector_size(struct vector *vec);
+int vector_capacity(struct vector *vec);
+void vector_reserve(struct vector *vec, int new_cap);
+void vector_shrink(struct vector *vec);
+
+void vector_clear(struct vector *vec);
+void vector_insert(struct vector *vec, int pos, int val);
+void vector_push_back(struct vector *vec, int val);
+void vector_pop_back(struct vector *vec);
+
+#endif
