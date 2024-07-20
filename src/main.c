10ee6fc1e149f8a3bc41630390787a9d88b824b1
Pavel Sytnik <pashasytnik@gmail.com>
2024-07-20 22:21:26 +0400

    Convert vector implementation into macro-based

--- src/main.c
+++ src/main.c
@@ -7,14 +7,21 @@
 
 int main(void)
 {
-    short *vec = vector_create();
+    short *vec = NULL;
+    vector_create(vec);
 
     printf("Is empty: %d\n", vector_is_empty(vec));
     printf("Size: %d\n", vector_size(vec));
     printf("Capacity: %d\n", vector_capacity(vec));
 
+    vector_reserve(vec, 58);
+    printf("Capacity: %d\n", vector_capacity(vec));
+
+    /* NOT SUPPOSED TO BE DONE LIKE THAT BECAUSE THE SIZE IS STILL ZERO */
+    vec[5] = 1785;
+    printf("vec[5]: %d\n", vec[5]);
+
     vector_destroy(vec);
-    vec = NULL;
 
     return 0;
 }
--- src/vector.h
+++ src/vector.h
@@ -1,13 +1,54 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-void *vector_create();
-void vector_destroy(void *vector);
+#include <stdlib.h>
 
-int vector_is_empty(void *vector);
-int vector_size(void *vector);
-int vector_capacity(void *vector);
-//void vector_reserve(struct vector *vec, int new_cap);
+struct vector_header {
+    int size;
+    int capacity;
+};
+
+#define vector_create(vector) do {                                         \
+                                                                           \
+    struct vector_header *metadata = malloc(sizeof(struct vector_header)); \
+    if (!metadata)                                                         \
+        break;                                                             \
+                                                                           \
+    metadata->size = 0;                                                    \
+    metadata->capacity = 0;                                                \
+                                                                           \
+    vector = ++metadata;                                                   \
+} while (0)
+
+#define vector_reserve(vector, new_capacity) do {                         \
+                                                                          \
+    struct vector_header *metadata = (struct vector_header*)vector - 1;   \
+                                                                          \
+    if (new_capacity <= metadata->capacity)                               \
+        break;                                                            \
+                                                                          \
+    void *new_memory = realloc(metadata, new_capacity * sizeof(*vector)); \
+    if (!new_memory)                                                      \
+        break;                                                            \
+                                                                          \
+    vector = (size_t)new_memory + sizeof(struct vector_header);           \
+    ((struct vector_header*)vector - 1)->capacity = new_capacity;         \
+} while (0)
+
+#define vector_destroy(vector) do {                      \
+    free((size_t)vector - sizeof(struct vector_header)); \
+    vector = NULL;                                       \
+} while (0)
+
+#define vector_is_empty(vector) \
+    (((struct vector_header*)vector - 1)->size == 0)
+
+#define vector_size(vector) \
+    (((struct vector_header*)vector - 1)->size)
+
+#define vector_capacity(vector) \
+    (((struct vector_header*)vector - 1)->capacity)
+
 //void vector_shrink(struct vector *vec);
 //
 //void vector_clear(struct vector *vec);
